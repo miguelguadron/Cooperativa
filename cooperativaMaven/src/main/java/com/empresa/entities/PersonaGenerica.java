e210b8c7686979f7a8cf955f3d5bb1c4f1f8d857
miguel.guadronusam <miguel.guadronusam@CDS2-USAM033.sv.cds>
2019-05-08 11:26:49 -0600

    Maven de cooperativa

---
+++ cooperativaMaven/src/main/java/com/empresa/entities/PersonaGenerica.java
@@ -0,0 +1,55 @@
+package com.empresa.entities;
+
+public class PersonaGenerica {
+	private String nombre;
+	private String apellidos;
+	private String numeroDocumento;
+	private Integer tiempoconocerse;
+	private Integer edad;
+	private String parentesco;
+	private Integer porcentaje;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getNumeroDocumento() {
+		return numeroDocumento;
+	}
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+	public Integer getTiempoconocerse() {
+		return tiempoconocerse;
+	}
+	public void setTiempoconocerse(Integer tiempoconocerse) {
+		this.tiempoconocerse = tiempoconocerse;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	public String getParentesco() {
+		return parentesco;
+	}
+	public void setParentesco(String parentesco) {
+		this.parentesco = parentesco;
+	}
+	public Integer getPorcentaje() {
+		return porcentaje;
+	}
+	public void setPorcentaje(Integer porcentaje) {
+		this.porcentaje = porcentaje;
+	}
+	
+	
+}
